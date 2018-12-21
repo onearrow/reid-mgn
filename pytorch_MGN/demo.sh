977aded39a61c47e775c9880178ee74371cbfb08
零尾 <lwplwf@qq.com>
2018-12-21 15:54:38 +0800

    Update demo.sh

--- pytorch_MGN/demo.sh
+++ pytorch_MGN/demo.sh
@@ -61,3 +61,6 @@
 # 2018.12.11 23:16->?,PReLU
 # CUDA_VISIBLE_DEVICES=0 python main.py --datadir /home/lwp/beednprojects/mgn/Market-1501-v15.09.15/ --batchid 8 --batchtest 16 --test_every 50 --epochs 400 --decay_type step_320_380 --loss 1*CrossEntropy+2*Triplet --margin 1.2 --save lwp-2018.12.11 --nGPU 1 --lr 2e-4 --optimizer ADAM --random_erasing --reset --re_rank --amsgrad
 # mAP: 0.9472 rank1: 0.9614 rank3: 0.9780 rank5: 0.9816 rank10: 0.9875 (Best: 0.9472 @epoch 350)
+
+# test-lwp
+# python main.py --datadir /home/lwp/beednprojects/mgn/Market-1501-v15.09.15/ --margin 1.2 --save sgd_0 --cpu --test_only --resume 0 --pre_train model/model_best.pt
